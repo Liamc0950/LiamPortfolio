32e80c9aed72115beabad1fcf1e34ca478a082e3
Liam Corley <liamcorley@Liams-Mac-mini.local>
2022-03-06 15:48:47 -0500

    Revert to old grid, added title overlays

--- src/pages/programming-gallery.js
+++ src/pages/programming-gallery.js
@@ -8,26 +8,77 @@ import '../components/base.css';
 import { GatsbyImage } from "gatsby-plugin-image"
 import { Row, Col } from 'react-bootstrap'
 
+import * as styles from './gallery.module.css'
 
 
 
 class RootIndex extends React.Component {
+  
   render() {
     const siteTitle = get(this, 'props.data.site.siteMetadata.title')
     const shows = get(this, 'props.data.allContentfulShow.edges')
 
+    const galleryRows = [];
+
+    const width = window.innerWidth;
+
+    var colLimit = 3;
+
+    if(width <= 400){
+      colLimit = 2;
+    }
+
+    //instantiate the first row as an array
+    galleryRows[0] = [];
+
+    for(let i = 0, j = 0, k=0; i<shows.length; i++){
+      //if the current galleryRow is not full
+      if(j < colLimit){
+        //add show[i] to the given galleryRow array
+        galleryRows[k].push(shows[i])
+        //and increment the column count for that row
+        j++;
+      }
+      else{
+        k++;
+        j=1;
+        //create an array for the new galleryRow
+        galleryRows[k] = [];
+        //then show[i] to the new galleryRow
+        galleryRows[k].push(shows[i])
+      }
+    }
+
     return (
       <Layout location={this.props.location}>
         <Helmet title={siteTitle} />
-          <Row className="g-0">
+          {galleryRows.map(row => (
+            <Row className="g-0">
+              {row.map(show=>(
+                <Col>
+                  <div className={styles.wrapper}>
+                    <Link to={"/shows/" + show.node.title + "/"}>
+                      <GatsbyImage image={show.node.coverImage.gatsbyImageData} alt={'alt text'}/>
+                      <div className={styles.overlay}>
+                        <div className={styles.title}>
+                          <h6 className='text-light'>{show.node.title}</h6>
+                        </div>
+                      </div>
+                    </Link>
+                  </div>
+                </Col>              
+              ))}
+            </Row>
+          ))}
+          {/* <Row>
             {shows.map(show=> (
-              <Col lg={4} md = {4} sm = {6} xs = {6}>
+              <Col lg={4} md = {4} sm = {6}>
                 <Link to={"/shows/" + show.node.title + "/"}>
                   <GatsbyImage image={show.node.coverImage.gatsbyImageData} alt={'alt text'}/>
                 </Link>
               </Col>
             ))}
-          </Row>
+          </Row> */}
       </Layout>
     )
   }
